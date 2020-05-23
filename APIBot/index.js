86dcebc4aacbd90c83c0d63fa25e5250c74e9625
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-05-24 01:37:38 +0530

    add cat handler

--- APIBot/index.js
+++ APIBot/index.js
@@ -3,9 +3,23 @@ const axios = require('axios');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const helpMessage = `Use the following commands
+
+/start - start the bot
+/help - help reference
+/fortunecookie - get a random fortune cookie
+/cat - get a random cat image
+/cat <text> - get a random cat image with text written on it
+`;
+
 bot.start(ctx => {
     bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-    ctx.reply('Welcome, to the Simple API Bot, ' + ctx.from.first_name + '! 👋');
+    ctx.reply('Welcome, to the Simple API Bot, ' + ctx.from.first_name + '! 👋\n');
+    ctx.reply(helpMessage);
+})
+
+bot.help(ctx => {
+    ctx.reply(helpMessage);
 })
 
 bot.command('fortunecookie', ctx => {
@@ -18,6 +32,30 @@ bot.command('fortunecookie', ctx => {
         }).catch((err) => {
             console.log(err);
         });
+})
+
+bot.command('cat', async ctx => {
+    const input = ctx.message.text;
+    const inputList = input.split(' ');
+
+    if (inputList.length === 1) {
+        try {
+            const res = await axios.get('http://aws.random.cat/meow');
+            bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
+            bot.telegram.sendPhoto(ctx.chat.id, res.data.file, { reply_to_message_id: ctx.message.message_id })
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        try {
+            inputList.shift();
+            const text = inputList.join(' ');
+            bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
+            ctx.replyWithPhoto(`https://cataas.com/cat/says/${text}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 })
 
 bot.launch()

f55e392849531fb28018adaf92b4014418ae6cf3
Rohan Gupta <rohan.gupta2@oyorooms.com>
2020-05-25 14:17:08 +0530

    modify start and add prices action cb

--- CryptoBot/index.js
+++ CryptoBot/index.js
@@ -5,68 +5,52 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(ctx => {
     bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-    ctx.reply('Welcome to the Crypto Bot, ' + ctx.from.first_name + '! 😀');
+    renderStartBody(ctx);
 })
 
-bot.command('menu', ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, 'Main Menu', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: 'See fruits list', callback_data: 'fruits' },
-                    { text: 'See meats list', callback_data: 'meats' }
-                ]
-            ]
-        }
-    });
-})
-
-bot.action('fruits', ctx => {
+bot.action('start', ctx => {
     ctx.deleteMessage();
     ctx.answerCbQuery();
-    bot.telegram.sendMessage(ctx.chat.id, 'List of fruits:\n\t-Apples\n\t-Oranges\n\t-Mangoes', {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: 'Back to Main Menu', callback_data: 'menu' }
-                ]
-            ]
-        }
-    });
+    renderStartBody(ctx);
 })
 
-bot.action('meats', ctx => {
+bot.action('prices', ctx => {
+    let message = `Get Price Information, Select one of the cryptocurrencies:`;
     ctx.deleteMessage();
     ctx.answerCbQuery();
-    bot.telegram.sendMessage(ctx.chat.id, 'List of meats:\n\t-Chicken\n\t-Fish\n\t-Pork', {
+    bot.telegram.sendMessage(ctx.chat.id, message, {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'Back to Main Menu', callback_data: 'menu' }
+                    { text: 'BTC', callback_data: 'price-BTC' },
+                    { text: 'ETH', callback_data: 'price-ETH' },
+                ],
+                [
+                    { text: 'BCH', callback_data: 'price-BCH' },
+                    { text: 'LTC', callback_data: 'price-LTC' },
+                ],
+                [
+                    { text: 'Back to Menu', callback_data: 'start' }
                 ]
             ]
         }
-    });
+    })
 })
 
-
-bot.action('menu', ctx => {
-    ctx.deleteMessage();
-    ctx.answerCbQuery();
-    bot.telegram.sendMessage(ctx.chat.id, 'Main Menu', {
+renderStartBody = ctx => {
+    const message = 'Welcome to the Crypto Bot, ' + ctx.from.first_name + '! 😀';
+    bot.telegram.sendMessage(ctx.chat.id, message, {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'See fruits list', callback_data: 'fruits' },
-                    { text: 'See meats list', callback_data: 'meats' }
+                    { text: 'Crypto Prices', callback_data: 'prices' }
+                ],
+                [
+                    { text: 'CoinMarketCap', url: 'https://coinmarketcap.com' }
                 ]
             ]
         }
-    });
-})
-// bot.action('one', ctx => {
-//     ctx.answerCbQuery();
-//     ctx.reply(ctx.from.first_name + ' you clicked button one.');
-// })
+    })
+}
 
 bot.launch();
